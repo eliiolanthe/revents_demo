b0d26f9a99722f17d47f2e702a1805599d6b5849
Elvira Paulin <eli.paulin@gmail.com>
2019-06-25 22:11:22 +0200

    Added mock data

--- src/features/event/EventDashboard/EventDashboard.jsx
+++ src/features/event/EventDashboard/EventDashboard.jsx
@@ -3,12 +3,14 @@ import { Grid, Button } from "semantic-ui-react";
 import EventList from "../EventList/EventList";
 import EventForm from "../EventForm/EventForm";
 
+import events from "../services/events";
+
 class EventDashboard extends Component {
   render() {
     return (
       <Grid>
         <Grid.Column width={10}>
-          <EventList />
+          <EventList events={events} />
         </Grid.Column>
         <Grid.Column width={6}>
           <Button positive content="Create Event" />
