dd33113d4dd14b5a2c23a7fc89fa8b02379ab90f
Elvira Paulin <eli.paulin@gmail.com>
2019-06-30 19:45:33 +0200

    Added router

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,16 +1,38 @@
 import React, { Component, Fragment } from 'react';
+import { Container } from 'semantic-ui-react';
+import { Route } from 'react-router-dom';
 import EventDashboard from '../../features/event/EventDashboard/EventDashboard';
 import NavBar from '../../features/nav/NavBar/NavBar';
-import { Container } from 'semantic-ui-react';
+import HomePage from '../../features/home/HomePage';
+import EventDetailedPage from '../../features/event/EventDetailed/EventDetailedPage';
+import PeopleDashboard from '../../features/user/PeopleDashboard/PeopleDashboard';
+import UserDetailedPage from '../../features/user/UserDetailed/UserDetailedPage';
+import SettingsDashboard from '../../features/user/UserSettings/SettingsDashboard';
+import EventForm from '../../features/event/EventForm/EventForm';
 
 class App extends Component {
   render() {
     return (
       <Fragment>
-        <NavBar />
-        <Container className="main">
-          <EventDashboard />
-        </Container>
+        {/* Looks for exact path to home page - which is now 'outside' our app container */}
+        <Route exact path="/" component={HomePage} />
+        {/* Looks for matches after '/' */}
+        <Route
+          path="/(.+)"
+          render={() => (
+            <Fragment>
+              <NavBar />
+              <Container className="main">
+                <Route path="/events" component={EventDashboard} />
+                <Route path="/events/:id" component={EventDetailedPage} />
+                <Route path="/people" component={PeopleDashboard} />
+                <Route path="/profile/:id" component={UserDetailedPage} />
+                <Route path="/settings" component={SettingsDashboard} />
+                <Route path="/createEvent" component={EventForm} />
+              </Container>
+            </Fragment>
+          )}
+        />
       </Fragment>
     );
   }
